d87f09b4d85be43dce715c4f8f990b1b17f6a102
Arisara Phongsai <aris.pw10@gmail.com>
2024-03-10 13:57:47 +0700

    create script line shopping

---
+++ lineshopping.js
@@ -0,0 +1,31 @@
+const clickProduct = () => {
+  document.querySelectorAll("a")[0].click();
+};
+
+const selectOption = () => {
+  document
+    .querySelectorAll("button[data-atd='variant-option1-button']")[0]
+    .click();
+};
+
+const increaseQuantity = () => {
+  document
+    .querySelectorAll("button[data-atd='increase-quantity-button']")[0]
+    .click();
+};
+
+const decreaseQuantity = () => {
+  document
+    .querySelectorAll("button[data-atd='decrease-quantity-button']")[0]
+    .click();
+};
+
+const buynowButton = () => {
+  document.querySelector("button[data-atd='buy-now-button']").click();
+};
+
+const paymentDetail = () => {
+  document.querySelector("label[for*='shipment-option']").click();
+  document.querySelector("input[name='payment-option-list-radio-3']").click();
+  document.querySelector("button[data-atd='placeorder-button']").click();
+};
