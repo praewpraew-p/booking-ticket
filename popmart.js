9c26e27185014d6453ab16416ec230734758b492
Arisara Phongsai <aris.pw10@gmail.com>
2024-04-29 11:31:48 +0700

    init popmart note

---
+++ popmart.js
@@ -0,0 +1,13 @@
+// Pattern Link Checkout Product
+// spuId is ID
+// skuId find from productDetail api -> skus
+// name of the product is hexadecimal code, more info http://code.cside.com/3rdpage/us/url/converter.html
+// stay in the checkout page before selling start
+
+// example
+// https://www.popmart.com/th/checkout?spuId=788&skuId=1287&count=1&spuTitle=LABUBU%20%C3%97%20PRONOUNCE-BE%20FANCY%20NOW%20Vinyl%20Plush%20Doll
+// https://www.popmart.com/th/checkout?spuId=15&skuId=29&count=1&spuTitle=SKULLPANDA%20Ancient%20Castle%20Series
+// https://www.popmart.com/th/checkout?spuId=463&skuId=794&count=1&spuTitle=HACIPUPU%20In%20My%20Dream%20Series%20Figures
+// https://www.popmart.com/th/checkout?spuId=873&skuId=1393&count=1&spuTitle=JACKSON%20WANG%20MAGIC%20MAN%20SERIES%20FIGURES
+// https://www.popmart.com/th/checkout?spuId=918&skuId=1460&count=1&spuTitle=CRYBABY%20CHEER%20UP%2C%20BABY%21%20SERIES-Plush%20Doll
+// https://www.popmart.com/th/checkout?spuId=919&skuId=1461&count=1&spuTitle=CRYBABY%20CHEER%20UP%2C%20BABY%21%20SERIES-Plush%20Doll%20Pendant
