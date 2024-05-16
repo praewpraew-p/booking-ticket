f30107a57e1d4b61e6e2a62579c237e59c78f961
Arisara Phongsai <aris.pw10@gmail.com>
2024-05-16 09:03:21 +0700

    increase amount in lazada

--- shopping/lazada.js
+++ shopping/lazada.js
@@ -2,6 +2,14 @@ const buynow = () => {
   document.getElementsByClassName("add-to-cart-buy-now-btn")[0].click();
 };
 
+const increaseAmount = () => {
+  document
+    .getElementsByClassName(
+      "next-number-picker-handler next-number-picker-handler-up "
+    )[0]
+    .click();
+};
+
 const checkout = () => {
   document
     .querySelector("div[style*='background-color: rgb(245, 114, 36)']")
